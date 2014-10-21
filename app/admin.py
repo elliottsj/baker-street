0c2ab560efe1ae7552503384c56202f46430685a
Spencer Elliott <me@elliottsj.com>
2014-10-21 19:44:46 -0400

    Add columns & filtering to question admin page

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'document')
+    list_filter = ['document']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Document on which this question was asked', {'fields': ['document']})
