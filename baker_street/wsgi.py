2a080a7d84803db2d9acc02fb75d568cc2db868c
Spencer Elliott <me@elliottsj.com>
2014-10-20 22:54:54 -0400

    Initialize Django

---
+++ baker_street/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for baker_street project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baker_street.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
