852d337b93d3040e2dae6a35327296b84136898f
Spencer Elliott <me@elliottsj.com>
2014-10-21 21:26:04 -0400

    Simplify template rendering

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader, RequestContext
 
 
 def index(request):
-    template = loader.get_template('dashboard/index.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+    return render(request, 'dashboard/index.html')
