b4776bd2ef5bcbd0a7777600012685932f07da61
Spencer Elliott <me@elliottsj.com>
2014-11-01 15:52:40 -0400

    Update requirements
    Switch to whitenoise

--- baker_street/wsgi.py
+++ baker_street/wsgi.py
@@ -11,6 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baker_street.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
