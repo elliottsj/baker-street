9e83799b605a42a686513ca8dc0923e49e7a8c74
Jonathan Webb <jwebbed@bell.net>
2014-11-03 15:46:28 -0500

    Effectively wrote an interface for the object

---
+++ api/sleuthing/documents.py
@@ -0,0 +1,25 @@
+class Document:
+
+    def __init__(self):
+        pass # Spencer can deal with this
+
+    def affinity(self):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+    def __ge__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __le__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __ne__(self, other):
+        pass
