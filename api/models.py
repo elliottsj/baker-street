6f2682d575441775fe3740571655f109f3e24872
Spencer Elliott <me@elliottsj.com>
2014-11-03 13:49:21 -0500

    Add Evidence model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+import pywatson.answer.evidence
 
 
 class Page(models.Model):
@@ -14,11 +15,31 @@ class Page(models.Model):
 
 class Document(models.Model):
     """A document in the corpus."""
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     publish_date = models.DateField(null=True)
 
     def __str__(self):
         return self.title
+
+
+class Evidence(models.Model):
+    # pywatson.answer.evidence.Evidence attributes
+    title = models.CharField(max_length=255)
+    copyright = models.CharField(max_length=255)
+    external_id = models.CharField(max_length=255)
+    terms_of_use = models.CharField(max_length=255)
+    document_path = models.CharField(max_length=255)
+    text = models.TextField()
+    confidence = models.FloatField()
+
+    # pywatson.answer.evidence.MetadataMap attributes
+    original_file = models.CharField(max_length=255)
+    deepqa_id = models.CharField(max_length=255)
+    corpus_name = models.CharField(max_length=255)
+    docno = models.CharField(max_length=255)
+    corpus_plus_docno = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=255)
+
 
 
 class Question(models.Model):
