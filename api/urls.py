789f7e4ade1e734c85333d78acb7cf4aceb3e678
Spencer Elliott <me@elliottsj.com>
2014-11-04 14:09:20 -0500

    Remove trailing slash from router

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from api import views
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', views.UserViewSet)
 router.register(r'documents', views.DocumentViewSet)
 router.register(r'questions', views.QuestionViewSet)
