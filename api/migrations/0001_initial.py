226e0eb0e2f12cc2268dd445c6df6165f7ad4934
Spencer Elliott <me@elliottsj.com>
2014-11-04 14:16:55 -0500

    Redo migrations

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -11,11 +13,74 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(verbose_name='last login', default=django.utils.timezone.now)),
+                ('first_name', models.CharField(max_length=30, blank=True, verbose_name='first name')),
+                ('last_name', models.CharField(max_length=30, blank=True, verbose_name='last name')),
+                ('email', models.EmailField(unique=True, max_length=75, verbose_name='email address')),
+                ('is_staff', models.BooleanField(help_text='Designates whether the user can log into this admin site.', verbose_name='staff status', default=False)),
+                ('is_active', models.BooleanField(help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active', default=True)),
+                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Context',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('value', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Document',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=255)),
                 ('publish_date', models.DateField(null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Evidence',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('copyright', models.CharField(max_length=255)),
+                ('external_id', models.CharField(max_length=255)),
+                ('terms_of_use', models.CharField(max_length=255)),
+                ('document_path', models.CharField(max_length=255)),
+                ('text', models.TextField()),
+                ('confidence', models.FloatField()),
+                ('original_file', models.CharField(max_length=255)),
+                ('deepqa_id', models.CharField(max_length=255)),
+                ('corpus_name', models.CharField(max_length=255)),
+                ('docno', models.CharField(max_length=255)),
+                ('corpus_plus_docno', models.CharField(max_length=255)),
+                ('file_name', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Page',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('url', models.TextField()),
+                ('title', models.TextField()),
+                ('content', models.TextField()),
+                ('document', models.OneToOneField(to='api.Document')),
             ],
             options={
             },
@@ -24,12 +89,41 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Question',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('question_text', models.TextField()),
                 ('document', models.ForeignKey(to='api.Document', null=True)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ResearchSession',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='page',
+            name='research_session',
+            field=models.ForeignKey(to='api.ResearchSession'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='page',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='context',
+            name='research_session',
+            field=models.ForeignKey(to='api.ResearchSession'),
+            preserve_default=True,
         ),
     ]
