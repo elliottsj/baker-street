e365bcddb4a9ed7c58f6e4588a30a9c148ed35e1
Jonathan Webb <jonathan.webb@mail.utoronto.ca>
2014-11-04 21:03:20 -0500

    WGET All of wikipedia

--- api/views.py
+++ api/views.py
@@ -29,7 +29,6 @@ class AuthViewSet(viewsets.ModelViewSet):
     serializer_class = AuthTokenSerializer
 
     # POST /users/sign_in.json
-    @csrf_exempt
     @list_route(methods=['POST'])
     def sign_in(self, request, format=None):
         serializer = self.serializer_class(data=request.DATA)
