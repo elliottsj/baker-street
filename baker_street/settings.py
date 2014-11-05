82088ffa2a13dceb56c23d446da18d9695eb0580
Jonathan Webb <jonathan.webb@mail.utoronto.ca>
2014-11-04 20:42:42 -0500

    nope

--- baker_street/settings.py
+++ baker_street/settings.py
@@ -141,7 +141,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'PAGINATE_BY': 10,
 }
 
