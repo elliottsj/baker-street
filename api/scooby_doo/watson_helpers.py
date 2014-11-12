7490f2c9df7ef4528dcde264c9e5c59fc3ba028b
Jonathan Webb <jwebbed@bell.net>
2014-11-11 23:16:08 -0500

    stuff

--- api/scooby_doo/watson_helpers.py
+++ api/scooby_doo/watson_helpers.py
@@ -52,3 +52,52 @@ if __name__ == '__main__':
     objs = get_documents("G. (Re), 2010 CanLII 5993 (ON CCB)")
     x = [x.title for x in objs]
     print(x)
+
+
+### Make this a migration somehow
+def populate():
+    canlii = CanLII("zxxdp6fyt5fatyfv44smrsbw")
+    case_dbs = canlii.case_databases()
+    legis_dbs = canlii.legislation_databases()
+
+    for db in case_dbs:
+        for case in db:
+            api.models.CanLIIDocument.objects.create(title=case.title, documentId=case.caseId,
+                                                     databaseId=case.databaseId, populated=False)
+
+    for db in legis_dbs:
+        for legis in db:
+            api.models.CanLIIDocument.objects.create(title=legis.title, documentId=legis.legislationId,
+                                                     databaseId=legis.databaseId, populated=False)
+
+
+def search(title):
+    models = CanLIIDocument.objects.filter(title=title)
+
+    ## Attempt to coax out some documents
+    if (len(models) == 0):
+        words = title.split()
+        while(len(models) == 0 and len(words) > 0):
+            words.pop()
+            s = words[0]
+            for i in range(1, len(words)):
+                s+= ' ' + words[i]
+            models = CanLIIDocument.objects.filter(title=title)
+
+    # Say fuck it
+    if (len(models) == 0):
+        return None
+
+    # Deal with too many results, probably by crying, hopefully with a fancy algorithm
+    # I'm envisioning something that compares based on words starting from the begining
+    if (len(models) > 1):
+        pass
+
+    model = models[0]
+    # this will require knowing if it's legislation or a case, should deal with this
+    if not model.populated:
+        pass #remember to save when this is done
+
+    return model
+
+
