45b83b0a7d92ac44be0a0218148da70fd3779bf2
Jonathan Webb <jonathan.webb@mail.utoronto.ca>
2014-11-22 01:59:37 -0500

    I think I'm a bad coder

--- baker_street/scooby_doo/watson_helpers.py
+++ baker_street/scooby_doo/watson_helpers.py
@@ -8,13 +8,13 @@ from baker_street.exceptions import InvalidDocumentException
 import requests
 
 
-def call_watson(body, session):
+def call_watson(body, session, items=3, calls=1):
     watson = Watson(url='https://watson-wdc01.ihost.com/instance/507/deepqa/v1',
                     username='ut_student5', password='9JwXacPH')
     context = getContext(session)
-    text = assertion(body, False, context, 1)[0]
+    text = assertion(body, False, context, calls)[0]
 
-    question = WatsonQuestion(text, formatted_answer=True, items=5, context=context,
+    question = WatsonQuestion(text, formatted_answer=True, items=items, context=context,
                           evidence_request= { "items": 2, "profile" : "yes"})
 
     answer = watson.ask_question(text, question=question)
@@ -24,14 +24,14 @@ def search_db(title):
     r = CanLIIDocument.search(title)
     return r.content
 
-def generate_questions_and_call_watson(t, session):
+def generate_questions_and_call_watson(t, session, items, calls):
     body = search_db(t)
 
-    answer = call_watson(body, session)
+    answer = call_watson(body, session, items, calls)
     return answer.evidence_list
 
-def get_documents(t, session):
-    evidence = generate_questions_and_call_watson(t, session)
+def get_documents(t, session, items=3, calls=1):
+    evidence = generate_questions_and_call_watson(t, session, items, calls)
     canlii = CanLII("zxxdp6fyt5fatyfv44smrsbw")
     l = []
     for e in evidence:
@@ -47,6 +47,9 @@ def get_documents(t, session):
             pass
 
     return l
+
+def backgroundUpdate(session):
+    
 
 if __name__ == '__main__':
     objs = get_documents("G. (Re), 2010 CanLII 5993 (ON CCB)")
--- baker_street/settings.py
+++ baker_street/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'rest_framework.authtoken',
     'django_extensions',
     'baker_street',
+    'kombu.transport.django',
 )
 
 MIDDLEWARE_CLASSES = (
