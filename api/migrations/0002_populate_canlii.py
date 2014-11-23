d1ff5b8a28c03e8ac0f0c3a8b695e3ef58fce747
Spencer Elliott <me@elliottsj.com>
2014-11-22 19:23:12 -0500

    Optimize imports & whitespace

--- api/migrations/0002_populate_canlii.py
+++ api/migrations/0002_populate_canlii.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 from pycanlii.canlii import CanLII
 
+
 def populate_canlii(apps, scheme_editor):
-    canlii = CanLII("zxxdp6fyt5fatyfv44smrsbw")
+    canlii = CanLII('zxxdp6fyt5fatyfv44smrsbw')
     case_dbs = canlii.case_databases()
     legis_dbs = canlii.legislation_databases()
-    CanLIIDocument = apps.get_model("api", "CanLIIDocument")
+    CanLIIDocument = apps.get_model('api', 'CanLIIDocument')
 
     for db in case_dbs:
         for case in db:
             CanLIIDocument.objects.create(title=case.title, documentId=case.caseId, databaseId=case.databaseId,
                                           citation=case.citation, type=0, populated=False)
-
 
     for db in legis_dbs:
         for legis in db:
